305345917bb6c003ef27d3a3c5ac4644bc347024
Hendi Santika <hendisantika@gmail.com>
2018-12-15 09:10:17 +0700

    #7 - Add project_repository.js file

---
+++ project_repository.js
@@ -0,0 +1,13 @@
+class ProjectRepository {
+    constructor(dao) {
+        this.dao = dao
+    }
+
+    createTable() {
+        const sql = `
+    CREATE TABLE IF NOT EXISTS projects (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT)`;
+        return this.dao.run(sql)
+    }
+}
